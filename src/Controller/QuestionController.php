568d0ae9b69dc28e29e84a75b8251a6e428f983f
Ramón <ramon.montalban@outlook.es>
2023-03-02 08:21:52 +0100

    first commit

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('OMG! My first page already! Wooo!');
+    }
+
+    /**
+     * @Route("/questions/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the question: "%s"!',
+            ucwords(str_replace('-', ' ', $slug))
+        ));
+    }
+
+}
